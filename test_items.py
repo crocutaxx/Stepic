89a31e82f3ec0cf51429965b25ec05d0006e6519
Valery <85788550+crocutaxx@users.noreply.github.com>
2023-12-30 23:04:00 +0600

    add wait 30 sec

--- test_items.py
+++ test_items.py
@@ -3,6 +3,6 @@ from selenium.webdriver.support import expected_conditions as EC
 def test_button(browser):
     BUTTON = ('xpath', "(//button[@type='submit'])[2]")
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    element = WebDriverWait(browser, 10).until(EC.visibility_of_element_located(BUTTON))
+    element = WebDriverWait(browser, 30).until(EC.visibility_of_element_located(BUTTON))
     assert element
 
