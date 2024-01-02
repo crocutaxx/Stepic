f97fed2d0b9cdaf5fedfa884e6ec41a82ba36877
Valery <85788550+crocutaxx@users.noreply.github.com>
2024-01-02 13:13:09 +0600

    add object page

--- test_items.py
+++ test_items.py
@@ -1,8 +1,11 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import time
 def test_button(browser):
     BUTTON = ('xpath', "(//button[@type='submit'])[2]")
+
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    time.sleep(10)
     element = WebDriverWait(browser, 30).until(EC.visibility_of_element_located(BUTTON))
     assert element
 
